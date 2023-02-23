020e7efca96cd78b81121b25da4651ee1222ea15
Aviv Haim <avivos40@gmail.com>
2023-02-23 14:35:56 +0200

    Fix migration

--- server/db/deel-migrations/20230223110417-conversation_user_type.js
+++ server/db/deel-migrations/20230223110417-conversation_user_type.js
@@ -1,8 +1,14 @@
 exports.up = knex => {
   return knex.schema.withSchema('deel').raw(
     `TRUNCATE TABLE deel.conversations, deel.conversation_content; 
-  CREATE TYPE deel.conversation_user_types AS ENUM ('DEEL_ADMIN', 'DEEL_USER');
-  ALTER TABLE deel.conversations ADD COLUMN user_ref INT NOT NULL, ADD COLUMN user_type deel.conversation_user_types NOT NULL ;`)
+    DO $$
+    BEGIN
+        CREATE TYPE deel.conversation_user_types AS ENUM ('DEEL_ADMIN', 'DEEL_USER');
+    EXCEPTION
+        WHEN duplicate_object THEN
+            NULL; --ignore
+    END; $$;
+  ALTER TABLE deel.conversations ADD COLUMN IF NOT EXISTS user_ref INT NOT NULL, ADD COLUMN IF NOT EXISTS user_type deel.conversation_user_types NOT NULL ;`)
     .raw(`TRUNCATE TABLE deel.page_chunks,  deel.page_tsvs;`)
     .raw(`CREATE OR REPLACE FUNCTION public.dot_product(vector1 numeric[], vector2 numeric[])
     RETURNS numeric
